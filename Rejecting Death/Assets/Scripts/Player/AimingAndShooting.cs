1f203c161583f756696914ba9296d1867dc9bc5a
Thunder19 <braydenhamous@gmail.com>
2021-02-09 11:49:56 -0600

    a

--- Rejecting Death/Assets/Scripts/Player/AimingAndShooting.cs
+++ Rejecting Death/Assets/Scripts/Player/AimingAndShooting.cs
@@ -11,6 +11,7 @@ public class AimingAndShooting : MonoBehaviour
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 >>>>>>> parent of a475ba1... 5
 { 
@@ -44,6 +45,8 @@ public class AimingAndShooting : MonoBehaviour
 >>>>>>> parent of 86a2972... Revert "7"
 =======
 >>>>>>> parent of 86a2972... Revert "7"
+=======
+>>>>>>> parent of 86a2972... Revert "7"
 {
     public GameObject crosshair;
     private Vector3 target;
@@ -65,12 +68,15 @@ public class AimingAndShooting : MonoBehaviour
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 >>>>>>> parent of 86a2972... Revert "7"
 =======
 >>>>>>> parent of 86a2972... Revert "7"
 =======
 =======
 >>>>>>> parent of a475ba1... 5
+>>>>>>> parent of 86a2972... Revert "7"
+=======
 >>>>>>> parent of 86a2972... Revert "7"
 =======
 >>>>>>> parent of 86a2972... Revert "7"
