8af629b20eb22519689b2a2052662d316dee07b8
mutabadrk7 <54857665+mutabadrk7@users.noreply.github.com>
2021-02-12 20:03:37 -0800

    update scripts and assets

---
+++ Rejecting Death/Assets/Scripts/interactables/crumblefloor.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class crumblefloor : MonoBehaviour
+{
+
+    private Rigidbody2D rb2d;
+    public float fallDelay;
+    public float respawnDelay;
+    private Vector2 startPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        startPos = new Vector2(transform.position.x, transform.position.y);
+        GetComponent<EdgeCollider2D>().isTrigger = false;
+    }
+
+    // Update is called once per frame
+     void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+
+        if (col.collider.CompareTag("Player"))
+        {
+            StartCoroutine(Fall());
+            
+        }    
+    }
+    private void OnCollisionExit2D(Collision2D col)
+    {
+        if (GetComponent<EdgeCollider2D>().isTrigger == true )
+        { 
+            
+        }
+    }
+    IEnumerator Fall()
+    {
+        yield return new WaitForSeconds(fallDelay);
+        rb2d.isKinematic = false;
+        GetComponent<EdgeCollider2D>().isTrigger = true;
+       //yield return new WaitForSeconds(respawnDelay);
+        //StartCoroutine(Respawn());
+        yield return 0;
+
+
+
+    }
+    IEnumerator Respawn()
+    {
+        
+        
+        rb2d.isKinematic = true;
+        
+        GetComponent<EdgeCollider2D>().isTrigger = false;
+        transform.position = startPos;
+        
+        
+        yield return 0;
+    }
+}
---
+++ Rejecting Death/Assets/Scripts/interactables/key.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class key : MonoBehaviour
+{
+
+    private bool isFollowing;
+
+    public float followSpeed;
+
+    public Transform folowTarget;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isFollowing = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isFollowing)
+        {
+            transform.position = Vector3.Lerp(transform.position, folowTarget.position, followSpeed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+           
+            if (!isFollowing)
+            {
+                Movement thePlayer = FindObjectOfType<Movement>();
+
+                folowTarget = thePlayer.keyFollowPoint;
+                thePlayer.followingkey = this;
+                isFollowing = true;
+            }
+        }
+    }
+}
