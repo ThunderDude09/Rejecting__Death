db662dcc76c1ffc9a9220aa576b9c6aa008a2f85
Thunder19 <braydenhamous@gmail.com>
2021-02-23 16:17:38 -0600

    13

---
+++ Rejecting Death/Assets/projectileCollision.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projectileCollision : MonoBehaviour
+{
+    //public Transform explosionPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 1.0f);
+    }
+
+    // Update is called once per frame
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+}
