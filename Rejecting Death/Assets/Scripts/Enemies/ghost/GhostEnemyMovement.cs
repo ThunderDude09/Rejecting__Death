fc01775d0b7e3f9acefe529c9f8d8d56813db628
Thunder19 <braydenhamous@gmail.com>
2021-02-24 21:13:22 -0600

    14

---
+++ Rejecting Death/Assets/Scripts/Enemies/ghost/GhostEnemyMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostEnemyMovement : MonoBehaviour
+{
+    private Movement player;
+
+    public float MoveSpeed;
+
+    public float playerRange;
+
+    public LayerMask playerLayer;
+
+    public bool playerInRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<Movement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerInRange = Physics2D.OverlapCircle(transform.position, playerRange, playerLayer);
+
+        if (playerInRange)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, MoveSpeed * Time.deltaTime);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawSphere(transform.position, playerRange);
+    }
+}
