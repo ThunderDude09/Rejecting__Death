8144a4792c79365b64a2d569172c11a77d727db4
mutabadrk7 <54857665+mutabadrk7@users.noreply.github.com>
2021-03-17 15:38:12 -0700

    update

--- Rejecting Death/Assets/Scripts/interactables/Door.cs
+++ Rejecting Death/Assets/Scripts/interactables/Door.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class Door : MonoBehaviour
 {
     private Movement thePlayer;
+    public Animator door;
+    [SerializeField] private key.KeyType keyType;
 
-    public SpriteRenderer theSR;
-    public Sprite doorOpenSprite;
+    public key.KeyType GetKeyType()
+    {
+        return keyType;
+    }
+    
     key followingkey;
-    public bool doorOpen, waitingtoOpen;
+    public bool  waitingtoOpen;
     int scoremod = 1;
     // Start is called before the first frame update
     void Start()
@@ -26,30 +31,36 @@ public class Door : MonoBehaviour
             {
                 waitingtoOpen = false;
 
-                doorOpen = true;
-
-
-                theSR.sprite = doorOpenSprite;
-
                 thePlayer.followingkey.gameObject.SetActive(false);
                 thePlayer.followingkey = null;
 
-                GameManager.instance.AddScore(scoremod);
+                door.SetBool("Open", true);
 
-                 
+
+
+
+
+                
+                GameObject.Destroy(gameObject);
             }
         }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        
         if(collision.CompareTag("Player"))
         {
             if (thePlayer.followingkey != null)
             {
+                if (keyType == thePlayer.followingkey.GetComponent<key>().keyType)
                 thePlayer.followingkey.folowTarget = transform;
                 waitingtoOpen = true;
             }
         }
     }
+
+
+
+    
 }
