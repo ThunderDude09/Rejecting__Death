2218017c349b18b377a9fb3105ab85ca154835e4
Thunder19 <braydenhamous@gmail.com>
2021-03-28 23:52:44 -0500

    21

---
+++ Rejecting Death/Assets/Scripts/Enemies/Skeleton/Skeleton_Movement.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skeleton_Movement : MonoBehaviour
+{
+    public Transform player2;
+
+    public float MoveSpeed2;
+
+    public float playerRange2;
+
+    public LayerMask playerLayer2;
+
+    public bool playerInRange2;
+
+    private Vector2 Emovement2;
+
+    private Rigidbody2D erb2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //player = FindObjectOfType<Movement>();
+        erb2 = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerInRange2 = Physics2D.OverlapCircle(transform.position, playerRange2, playerLayer2);
+
+
+
+        if (playerInRange2)
+        {
+            //transform.position = Vector3.MoveTowards(transform.position, player.transform.position, MoveSpeed * Time.deltaTime);
+            Vector3 Pldirection = player2.position - transform.position;
+            Pldirection.Normalize();
+            Emovement2 = Pldirection;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (playerInRange2)
+        {
+            moveGEnemy(Emovement2);
+        }
+    }
+
+
+    void moveGEnemy(Vector2 Pldirection)
+    {
+        //erb2.MovePosition((Vector2)transform.position + (Pldirection * MoveSpeed2 * Time.deltaTime));
+        
+
+        if(transform.position.x < player2.position.x)
+        {
+            erb2.velocity = new Vector2(MoveSpeed2, erb2.velocity.y);
+        }
+        else
+        {
+            erb2.velocity = new Vector2(-MoveSpeed2, erb2.velocity.y);
+        }
+
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawSphere(transform.position, playerRange2);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            erb2.AddForce(Vector2.up * 300f);
+        }
+    }
+}
---
+++ Rejecting Death/Assets/Scripts/Falling_Icicle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Falling_Icicle : MonoBehaviour
+{
+    Rigidbody2D r_b;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        r_b = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Equals("Player"))
+            r_b.isKinematic = false;
+    }
+}
