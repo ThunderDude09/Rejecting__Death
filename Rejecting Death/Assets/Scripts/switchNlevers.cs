23b603d5ef44f717ff0d33f6411e6aa5a7836952
mutabadrk7 <54857665+mutabadrk7@users.noreply.github.com>
2021-04-01 21:58:05 -0700

    update

---
+++ Rejecting Death/Assets/Scripts/switchNlevers.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class switchNlevers : MonoBehaviour
+{
+    [SerializeField]
+    GameObject SwitchOn;
+
+    [SerializeField]
+    GameObject SwitchOff;
+
+    public bool isOn = false;
+
+    public int switchValue;
+
+    public GameObject puzzleManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<SpriteRenderer>().sprite = SwitchOff.GetComponent<SpriteRenderer>().sprite;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if (Input.GetKeyUp("up") || Input.GetKeyUp(KeyCode.W))
+            {
+                gameObject.GetComponent<SpriteRenderer>().sprite = SwitchOn.GetComponent<SpriteRenderer>().sprite;
+
+                puzzleManager.GetComponent<orderPuzzle>().getValue(switchValue);
+
+                isOn = true;
+            }
+        }
+    }
+}
