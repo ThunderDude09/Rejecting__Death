ffa156ed6bb378e86f33e74440253ed2107bf192
mutabadrk7 <54857665+mutabadrk7@users.noreply.github.com>
2021-04-27 10:21:31 -0700

    update

--- Rejecting Death/Assets/Scripts/crumblePlat.cs
+++ Rejecting Death/Assets/Scripts/crumblePlat.cs
@@ -4,21 +4,24 @@ using UnityEngine;
 
 public class crumblePlat : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    Rigidbody2D rb;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
-
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-       
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Invoke("DropPlatform", 0.5f);
+            Destroy(gameObject, 2f);
+        }
+    }
+
+    void DropPlatform()
+    {
+        rb.isKinematic = false;
     }
 }
