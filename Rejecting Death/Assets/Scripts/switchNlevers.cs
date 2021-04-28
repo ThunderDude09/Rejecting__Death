4a463898065b53f350c12fdd97a4908f01500872
mutabadrk7 <54857665+mutabadrk7@users.noreply.github.com>
2021-04-27 19:29:55 -0700

    update

--- Rejecting Death/Assets/Scripts/switchNlevers.cs
+++ Rejecting Death/Assets/Scripts/switchNlevers.cs
@@ -4,46 +4,5 @@ using UnityEngine;
 
 public class switchNlevers : MonoBehaviour
 {
-    [SerializeField]
-    GameObject SwitchOn;
-
-    [SerializeField]
-    GameObject SwitchOff;
-
-    public bool isOn = false;
-
-    public int switchValue;
-
-    public GameObject puzzleManager;
-
-    public AudioSource lever;
-    public AudioClip swish;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        gameObject.GetComponent<SpriteRenderer>().sprite = SwitchOff.GetComponent<SpriteRenderer>().sprite;
-        lever.clip = swish;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-       
-    }
-
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
-        {
-            if (Input.GetKeyUp("e") || Input.GetKeyUp(KeyCode.E))
-            {
-                gameObject.GetComponent<SpriteRenderer>().sprite = SwitchOn.GetComponent<SpriteRenderer>().sprite;
-
-                puzzleManager.GetComponent<orderPuzzle>().getValue(switchValue);
-                lever.Play();
-                isOn = true;
-            }
-        }
-    }
+    
 }
